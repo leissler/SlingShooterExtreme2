bc634889226924defa2ff7589edd5792042ee85d
Martin Leissler <martin@leissler.net>
2015-04-27 13:46:43 +0200

    Added slingshot mouse-over behavior

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	private GameObject launchPoint;
+
+	void Awake() {
+		Transform launchPointTrans = transform.Find("Launchpoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive(false);
+	}
+
+	void OnMouseEnter() {
+		//print ("Slingshot:MouseEnter");
+		launchPoint.SetActive(true);
+	}
+
+	void OnMouseExit() {
+		//print ("Slingshot:MouseExit");
+		launchPoint.SetActive(false);
+	}
+
+}
