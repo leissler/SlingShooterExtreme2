d8c7ac2f3dcad1ba7b4c954b952e02153494efaf
Martin Leissler <martin@leissler.net>
2015-06-01 16:21:08 +0200

    Added moving clouds background Added Castles Camera returns to slingshot after each shot

---
+++ Assets/Scripts/CloudCrafter.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudCrafter : MonoBehaviour {
+
+	// Inspector fields
+	public int numClouds = 40;
+
+	public Vector3 cloudPosMin;
+	public Vector3 cloudPosMax;
+
+	public float cloudScaleMin = 1.0f;
+	public float cloudScaleMax = 5.0f;
+
+	public float cloudSpeedMult = 0.5f;
+
+	public GameObject[] cloudPrefabs;
+
+
+	// Internal fields
+	private GameObject[] cloudInstances;
+
+	void Awake() {
+		// Create an array large enough to store all cloud instances
+		cloudInstances = new GameObject[numClouds];
+
+		// Find the cloud anchor in the hierarchy
+		GameObject anchor = GameObject.Find("Clouds");
+
+		// Iterate through array and create a cloud for each slot
+		GameObject cloud;
+		for (int i = 0; i < cloudInstances.Length; i++) {
+
+			// Randomly pick one of the cloud prefabs
+			int prefabNum = Random.Range(0,cloudPrefabs.Length);
+
+			// Create that instance
+			cloud = Instantiate(cloudPrefabs[prefabNum]);
+
+			// Position and scale the cloud (randomly)
+			Vector3 cPos = Vector3.zero;
+			cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
+			cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
+
+			float scaleU = Random.value;
+			float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
+
+			cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
+
+			cPos.z = 100 - 90*scaleU;
+
+			// Apply the changes to oour instance
+			cloud.transform.position = cPos;
+			cloud.transform.localScale = Vector3.one * scaleVal; 
+
+
+			// Make the cloud a child of our anchor
+			cloud.transform.parent = anchor.transform;
+
+			// Put the cloud into our instances array
+			cloudInstances[i] = cloud;
+
+		}
+	}
+
+	void Update() {
+		// Iterate through all cloud instance
+		foreach (GameObject cloud in cloudInstances) {
+			// Get the position and scale
+			float scaleVal = cloud.transform.localScale.x;
+			Vector3 cPos = cloud.transform.position;
+
+			cPos.x -= Time.deltaTime * cloudSpeedMult * scaleVal;
+
+			// Check if a clouds x position is too small
+			if(cPos.x < cloudPosMin.x) {
+
+				// set it to the maxium x position
+				cPos.x = cloudPosMax.x;
+			}
+
+			cloud.transform.position = cPos;
+		}
+	
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -11,6 +11,8 @@ public class FollowCam : MonoBehaviour {
 	private float camZ;
 	public float easing = 0.05f;
 
+	public Vector2 minXY;
+
 
 	void Awake() {
 		S = this;
@@ -19,22 +21,50 @@ public class FollowCam : MonoBehaviour {
 
 	void FixedUpdate() {
 
+		Vector3 destination;
+
 		// Check if the poi exists
 		if(poi == null) {
-			return;
+			// set the destination to the slingshot (zero  vector)
+			destination = Vector3.zero;
+		} else {
+			// else (there is a poi)
+
+			destination = poi.transform.position;
+
+			// is the poi a projectile ?
+			if(poi.tag == "Projectile") {
+
+				// check if it is resting (sleeping)
+				if(poi.GetComponent<Rigidbody>().IsSleeping()  ){
+			
+					// set the poi to default (null)
+					poi = null;
+					return;
+				}
+
+			}
 		}
 
-		Vector3 destination = poi.transform.position;
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.y = Mathf.Max (minXY.y, destination.y);
 
 		destination = Vector3.Lerp (transform.position, destination, easing);
 
 		destination.z = camZ;
 
 		transform.position = destination;
+
+		this.GetComponent<Camera>().orthographicSize = 10 + destination.y;
 	
 	}
 
 
 
 }
+
+
+
+
+
 
